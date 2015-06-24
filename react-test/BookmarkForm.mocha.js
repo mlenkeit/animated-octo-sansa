a53d71f7dbbb941fc99f03130e658ba71118b522
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2015-06-25 00:25:38 +0200

    added onSubmit prop to BookmarkForm
     - there is currently no data passed along with the events
     - trigger is tested with unit tests

--- react-test/BookmarkForm.mocha.js
+++ react-test/BookmarkForm.mocha.js
@@ -1,6 +1,7 @@
 require('./testdom')('<html><body></body></html>');
 var expect = require('chai').expect;
 var React = require('react/addons');
+var sinon = require('sinon');
 var TestUtils = React.addons.TestUtils;
 var BookmarkForm = require('./../react/BookmarkForm');
 
@@ -15,5 +16,25 @@ describe('BookmarkForm', function() {
 
   it('should render a form', function() {
     expect(form.getDOMNode().tagName.toLowerCase()).to.equal('form');
+  });
+
+  it('should do nothing when the form is submitted', function() {
+    TestUtils.Simulate.submit(form);
+  });
+
+  describe('with onSubmit handler', function() {
+
+    var handler;
+
+    beforeEach(function() {
+      handler = sinon.spy();
+      component = TestUtils.renderIntoDocument(<BookmarkForm onSubmit={handler} />);
+      form = TestUtils.findRenderedDOMComponentWithTag(component, 'form');
+    });
+
+    it('should invoke the onSubmit handler when the form is submitted', function() {
+      TestUtils.Simulate.submit(form);
+      expect(handler.callCount).to.equal(1, 'number of calls');
+    });
   });
 });
--- react/BookmarkForm.js
+++ react/BookmarkForm.js
@@ -1,8 +1,13 @@
 var React = require('react/addons');
 var BookmarkForm = React.createClass({
+  handleSubmit: function() {
+    if (this.props.onSubmit) {
+      this.props.onSubmit();
+    }
+  },
   render: function() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>Bookmark</label>
         <input type="text" />
         <input type="submit" />
