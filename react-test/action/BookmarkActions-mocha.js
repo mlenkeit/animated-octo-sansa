ad61a09462b948ac10b07f26746364dd774ff595
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2015-06-26 15:15:21 +0200

    post new bookmarks to backend

--- react-test/action/BookmarkActions-mocha.js
+++ react-test/action/BookmarkActions-mocha.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var expect = require('chai').expect;
+var nock = require('nock');
 var sinon = require('sinon');
 var Dispatcher = require('flux').Dispatcher;
 var BookmarkActions = require('./../../react/action/BookmarkActions');
@@ -10,21 +11,30 @@ var BookmarkActions = require('./../../react/action/BookmarkActions');
 describe('BookmarkActions', function() {
 
   var actions,
-      dispatcher;
+      dispatcher, host;
 
   beforeEach(function() {
     dispatcher = new Dispatcher();
     sinon.spy(dispatcher, 'dispatch');
-    actions = new BookmarkActions(dispatcher);
+    host = 'http://random-host.com';
+    actions = new BookmarkActions(dispatcher, host);
   });
 
-  describe('create', function() {
+  describe('create()', function() {
 
-    var url, tags;
+    var url, tags,
+        nockScope;
+
+    function assertCorrectActionPayload(action) {
+      expect(action).to.have.property('payload')
+        .that.deep.equals({url: url, tags: tags});
+    }
 
     beforeEach(function() {
       url = 'http://some-url.com';
       tags = '#someTag';
+
+      nockScope = nock(host);
     });
 
     it('dispatches a CreateBookmark action', function() {
@@ -34,6 +44,42 @@ describe('BookmarkActions', function() {
       expect(args[0]).to.have.property('name', 'CreateBookmark');
       expect(args[0]).to.have.property('payload')
         .that.deep.equals({url: url, tags: tags});
+    });
+
+    it('dispatches a CreateBookmarkSuccess action after a successful POST request to /api/bookmarks', function(done) {
+      nockScope.post('/api/bookmarks', {
+        url: url,
+        tags: tags
+      })
+      .reply(201);
+
+      dispatcher.register(function(action) {
+        if (action.name === 'CreateBookmarkSuccess') {
+          assertCorrectActionPayload(action);
+          nockScope.done();
+          done();
+        }
+      });
+
+      actions.create(url, tags);
+    });
+
+    it('disptaches a CreateBookmarkFailure action after a failed POST request to /api/bookmarks', function(done) {
+      nockScope.post('/api/bookmarks', {
+        url: url,
+        tags: tags
+      })
+      .reply(500);
+
+      dispatcher.register(function(action) {
+        if (action.name === 'CreateBookmarkFailure') {
+          assertCorrectActionPayload(action);
+          nockScope.done();
+          done();
+        }
+      });
+
+      actions.create(url, tags);
     });
   });
 
--- react/action/BookmarkActions.js
+++ react/action/BookmarkActions.js
@@ -1,6 +1,8 @@
 'use strict';
 
-module.exports = function(dispatcher) {
+var request = require('superagent');
+
+module.exports = function(dispatcher, host) {
 
   this.create = function(url, tags) {
     dispatcher.dispatch({
@@ -10,5 +12,29 @@ module.exports = function(dispatcher) {
         tags: tags
       }
     });
+
+    request.post(host + '/api/bookmarks')
+      .send({
+        url: url,
+        tags: tags
+      })
+      .end(function(err, res) {
+        if (err) {
+          return dispatcher.dispatch({
+            name: 'CreateBookmarkFailure',
+            payload: {
+              url: url,
+              tags: tags
+            }
+          });
+        }
+        dispatcher.dispatch({
+          name: 'CreateBookmarkSuccess',
+          payload: {
+            url: url,
+            tags: tags
+          }
+        });
+      });
   };
 };
