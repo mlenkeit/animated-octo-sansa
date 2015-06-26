a72ee2e7db73223672792837a86756423dcaf6ce
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2015-06-26 15:36:24 +0200

    added handler for CreateBookmark action to store
    the BookmarksStore gets the dispatcher injected and responds to the
    CreateBookmark action by optimistically adding the bookmark to the store.

--- react-test/store/BookmarksStore-mocha.js
+++ react-test/store/BookmarksStore-mocha.js
@@ -1,15 +1,18 @@
 var expect = require('chai').expect;
 var sinon = require('sinon');
 
+var Dispatcher = require('flux').Dispatcher;
 var EventEmitter = require('events').EventEmitter;
 var BookmarksStore = require('./../../react/store/BookmarksStore');
 
 describe('BookmarksStore', function() {
 
-  var store;
+  var store,
+      dispatcher;
 
   beforeEach(function() {
-    store = new BookmarksStore();
+    dispatcher = new Dispatcher();
+    store = new BookmarksStore(dispatcher);
   });
 
   it('is an EventEmitter', function() {
@@ -20,6 +23,23 @@ describe('BookmarksStore', function() {
 
     it('returns an empty array initially', function() {
       expect(store.getAll()).to.have.length.of(0);
+    });
+  });
+
+  describe('when a CreateBookmark action is dispatched', function() {
+
+    beforeEach(function() {
+      dispatcher.dispatch({
+        name: 'CreateBookmark',
+        payload: {
+          url: 'http://some-url.com',
+          tags: '#aTag'
+        }
+      });
+    });
+
+    it('adds the bookmark to the store (optimistically)', function() {
+      expect(store.getAll()).to.have.length.of(1);
     });
   });
 });
--- react/store/BookmarksStore.js
+++ react/store/BookmarksStore.js
@@ -1,10 +1,25 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-var Bookmarks = module.exports = function() {
+var Bookmarks = module.exports = function(dispatcher) {
+
+  var data = [];
+
+  dispatcher.register(function(action) {
+    var handlerName = 'handle' + action.name;
+    if (internals[handlerName]) {
+      internals[handlerName](action.payload);
+    }
+  });
+
+  var internals = {
+    handleCreateBookmark: function(payload) {
+      data.push(payload);
+    }
+  };
 
   this.getAll = function() {
-    return [];
-  }
+    return data;
+  };
 };
 util.inherits(Bookmarks, EventEmitter);
