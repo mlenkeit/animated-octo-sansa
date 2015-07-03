6f900920bd56da9d2e72bc520b005be836807305
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2015-07-03 21:42:30 +0200

    restructure tests to allow for constructor tests

--- backend-test/JSONStorage-mocha.js
+++ backend-test/JSONStorage-mocha.js
@@ -10,100 +10,56 @@ var JSONStorage = rewire('./../backend/JSONStorage');
 
 describe('JSONStorage', function() {
 
-  var app, req,
-      filename = 'bookmarks.json', filepath = __dirname + '/' + filename,
-      fsMock;
+  describe('when mounted', function() {
 
-  var STORAGE_DATA_FILE_NOT_FOUND = null;
+    var app, req,
+        filename = 'bookmarks.json', filepath = __dirname + '/' + filename,
+        fsMock;
 
-  beforeEach(function() {
-    fsMock = require('./fs-mock');
-    fsMock.__setStorageData = function(data) {
-      fsMock.readFileStub.withArgs(filepath).returns(data ? JSON.stringify(data) : null);
-    };
-    fsMock.__getStorageData = function() {
-      var data = fsMock.writeFileStub.args.length > 0 ?
-        fsMock.writeFileStub.args[fsMock.writeFileStub.args.length - 1][1] :
-        fsMock.readFileStub(filepath);
-      return data;
-    };
-    JSONStorage.__set__('fs', fsMock);
-    fsMock.__setStorageData([]);
-
-    app = express();
-    app.use('/', new JSONStorage({
-      filepath: filepath
-    }));
-  });
-
-  describe('GET /', function() {
+    var STORAGE_DATA_FILE_NOT_FOUND = null;
 
     beforeEach(function() {
-      req = request(app).get('/');
+      fsMock = require('./fs-mock');
+      fsMock.__setStorageData = function(data) {
+        fsMock.readFileStub.withArgs(filepath).returns(data ? JSON.stringify(data) : null);
+      };
+      fsMock.__getStorageData = function() {
+        var data = fsMock.writeFileStub.args.length > 0 ?
+          fsMock.writeFileStub.args[fsMock.writeFileStub.args.length - 1][1] :
+          fsMock.readFileStub(filepath);
+        return data;
+      };
+      JSONStorage.__set__('fs', fsMock);
+      fsMock.__setStorageData([]);
+
+      app = express();
+      app.use('/', new JSONStorage({
+        filepath: filepath
+      }));
     });
 
-    it('responds with 200', function(done) {
-      req.expect(200, done);
-    });
-
-    it('responds in json format', function(done) {
-      req.expect('Content-Type', /json/, done);
-    });
-
-    it('responds with an empty array by default', function(done) {
-      req.expect('[]', done);
-    });
-
-    it('responds with the content of the JSON file', function(done) {
-      var json = [{key: 1}, {key: 2}];
-      fsMock.__setStorageData(json);
-      req.expect(JSON.stringify(json), done);
-    });
-
-    describe('when the file is not found', function() {
+    describe('GET /', function() {
 
       beforeEach(function() {
-        fsMock.__setStorageData(STORAGE_DATA_FILE_NOT_FOUND);
+        req = request(app).get('/');
       });
 
-      it('responds with 500', function(done) {
-        req.expect(500, done);
-      });
-    });
-  });
-
-  describe('POST /', function() {
-
-    beforeEach(function() {
-      req = request(app).post('/').type('form');
-    });
-
-    describe('with valid payload', function() {
-
-      var payload;
-
-      beforeEach(function() {
-        payload = {
-          url: 'http://some-url.com',
-          tags: '#some #tag'
-        };
-        req.send(payload);
+      it('responds with 200', function(done) {
+        req.expect(200, done);
       });
 
-      it('responds with 201', function(done) {
-        req.expect(201, done);
+      it('responds in json format', function(done) {
+        req.expect('Content-Type', /json/, done);
       });
 
-      it('adds the bookmark to the end of the JSON file', function(done) {
-        fsMock.__setStorageData([{ url: 'initial', tags: '#initial' }]);
-        req.expect(function() {
-            var json = JSON.parse(fsMock.__getStorageData());
-            var lastRecord = json[json.length - 1];
-            expect(json).to.have.length.above(0);
-            expect(lastRecord.url).to.equal(payload.url);
-            expect(lastRecord.tags).to.equal(payload.tags);
-          })
-          .end(done);
+      it('responds with an empty array by default', function(done) {
+        req.expect('[]', done);
+      });
+
+      it('responds with the content of the JSON file', function(done) {
+        var json = [{key: 1}, {key: 2}];
+        fsMock.__setStorageData(json);
+        req.expect(JSON.stringify(json), done);
       });
 
       describe('when the file is not found', function() {
@@ -116,30 +72,77 @@ describe('JSONStorage', function() {
           req.expect(500, done);
         });
       });
-
-      describe('when writing to the file fails', function() {
-
-        beforeEach(function() {
-          fsMock.writeFileStub.returns(false);
-        });
-
-        it('responds with 500', function(done) {
-          req.expect(500, done);
-        });
-      });
     });
 
-    describe('with invalid payload', function() {
-
-      var payload;
+    describe('POST /', function() {
 
       beforeEach(function() {
-        payload = {};
-        req.send(payload);
+        req = request(app).post('/').type('form');
       });
 
-      it('responds with 400', function(done) {
-        req.expect(400, done);
+      describe('with valid payload', function() {
+
+        var payload;
+
+        beforeEach(function() {
+          payload = {
+            url: 'http://some-url.com',
+            tags: '#some #tag'
+          };
+          req.send(payload);
+        });
+
+        it('responds with 201', function(done) {
+          req.expect(201, done);
+        });
+
+        it('adds the bookmark to the end of the JSON file', function(done) {
+          fsMock.__setStorageData([{ url: 'initial', tags: '#initial' }]);
+          req.expect(function() {
+              var json = JSON.parse(fsMock.__getStorageData());
+              var lastRecord = json[json.length - 1];
+              expect(json).to.have.length.above(0);
+              expect(lastRecord.url).to.equal(payload.url);
+              expect(lastRecord.tags).to.equal(payload.tags);
+            })
+            .end(done);
+        });
+
+        describe('when the file is not found', function() {
+
+          beforeEach(function() {
+            fsMock.__setStorageData(STORAGE_DATA_FILE_NOT_FOUND);
+          });
+
+          it('responds with 500', function(done) {
+            req.expect(500, done);
+          });
+        });
+
+        describe('when writing to the file fails', function() {
+
+          beforeEach(function() {
+            fsMock.writeFileStub.returns(false);
+          });
+
+          it('responds with 500', function(done) {
+            req.expect(500, done);
+          });
+        });
+      });
+
+      describe('with invalid payload', function() {
+
+        var payload;
+
+        beforeEach(function() {
+          payload = {};
+          req.send(payload);
+        });
+
+        it('responds with 400', function(done) {
+          req.expect(400, done);
+        });
       });
     });
   });
