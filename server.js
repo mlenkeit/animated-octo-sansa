0659e510d54febe8e90f443c960f346bb31bc295
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2015-07-05 12:00:22 +0200

    change 'npm start' to run express server

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var App = require('./backend/App');
+var Logger = require('./backend/Logger');
+
+var logger = new Logger();
+var app = new App({
+  filepath: 'bookmarks.json',
+  logger: logger,
+  serve: __dirname + '/dist'
+});
+var port = 8080;
+app.listen(port, function() {
+  logger.info('Listening to port ' + port);
+});
